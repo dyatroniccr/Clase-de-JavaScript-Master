18f583fbb870f5a100d54fe036f48de4e97e8f9e
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-02-21 21:27:52 +0000

    Practica Master

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,3 +1,10 @@
 function isOldEnoughToDrink(age){
     // your code here	
+    if (age > 20) {
+        return true;
+    }
+    return false;
 }
+
+let output = isOldEnoughToDrink(9);
+console.log(output)
--- exercises/002-isOldEnoughToDrive/app.js
+++ exercises/002-isOldEnoughToDrive/app.js
@@ -1,3 +1,10 @@
 function isOldEnoughToDrive(age) {
   // your code here
+  if (age > 15) {
+    return true;
+  }
+  return false;
 }
+
+let output = isOldEnoughToDrive(15);
+console.log(output);
